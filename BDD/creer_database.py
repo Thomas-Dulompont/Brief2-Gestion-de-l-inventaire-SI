a5ac08845ba59c44ad21d2644def3d90929c2602
Thomas-Dulompont <thomasdulompont.1@gmail.com>
2022-10-04 09:59:06 +0200

    Ajout BDD

---
+++ BDD/creer_database.py
@@ -0,0 +1,69 @@
+import sqlite3
+
+connexion = sqlite3.connect("bdd.db")
+curseur = connexion.cursor()
+
+curseur.execute('''CREATE TABLE user
+                (
+                    id TEXT PRIMARY KEY,
+                    role INTERGER,
+                    nom TEXT,
+                    prenom TEXT,
+                    mot_de_passe TEXT
+                )
+''')
+
+curseur.execute('''CREATE TABLE type_ordi
+                (
+                    id INTEGER PRIMARY KEY,
+                    marque TEXT,
+                    processeur TEXT,
+                    carte_graphique TEXT,
+                    ram INTEGER,
+                    disque INTEGER
+                )
+''')
+
+curseur.execute('''CREATE TABLE carnet_pret
+                (
+                    reference_pc TEXT PRIMARY KEY,
+                    id_user INTEGER,
+                    id_pc INTEGER,
+                    FOREIGN KEY (id_user)
+                        REFERENCES user(id)
+                        ON DELETE CASCADE,
+                    FOREIGN KEY (id_pc)
+                        REFERENCES type_ordi(id)
+                        ON DELETE CASCADE
+                )
+''')
+
+curseur.execute('''CREATE TABLE ticket
+                (
+                    id INTEGER PRIMARY KEY,
+                    date INTEGER,
+                    id_ref_pret TEXT,
+                    status INTEGER,
+                    message TEXT,
+                    FOREIGN KEY (id_ref_pret)
+                        REFERENCES carnet_pret(reference_pc)
+                        ON DELETE CASCADE
+                )
+''')
+
+curseur.execute('''CREATE TABLE chat_tickets
+                (
+                    date INTEGER,
+                    id_ticket INTEGER,
+                    auteur TEXT,
+                    message TEXT,
+                    FOREIGN KEY (id_ticket)
+                        REFERENCES ticket(id)
+                        ON DELETE CASCADE,
+                    FOREIGN KEY (auteur)
+                        REFERENCES user(id)
+                        ON DELETE CASCADE
+                )
+''')
+
+connexion.commit()
