49d6179e181c7d6df677d18a65017cdae552ea98
Wilbert Kevin <92655178+Wilbert-kevin@users.noreply.github.com>
2022-10-04 15:07:18 +0200

    ajout

---
+++ Functions.py
@@ -0,0 +1,32 @@
+#on importe
+import sqlite3
+
+#Fonction
+def create_user(is_admin):
+    nom = input ("Entrez votre nom: ")
+    prenom = input ("Entrez votre prenom: ")
+    mdp = input ("Entrez votre mot passe : ")
+    crud.
+create_user(False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
