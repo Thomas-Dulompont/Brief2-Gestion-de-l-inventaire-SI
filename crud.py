0f617a5690ee133f93813c6b79b42fafb9b72c6d
Thomas-Dulompont <thomasdulompont.1@gmail.com>
2022-10-04 12:05:05 +0200

    Add func in crud

--- crud.py
+++ crud.py
@@ -1,4 +1,17 @@
 import sqlite3
 
-connexion = sqlite3.connect("./BDD/bdd.db")
-curseur = connexion.cursor()
+def create_user(is_admin, nom, prenom, mdp):
+    connexion = sqlite3.connect("./BDD/bdd.db")
+    curseur = connexion.cursor()
+    if is_admin:
+        curseur.execute("INSERT INTO user VALUES(?, ?, ?, ?, ?)", (None, 1, nom, prenom, mdp))
+    else:
+        curseur.execute("INSERT INTO user VALUES(?, ?, ?, ?, ?)", (None, 0, nom, prenom, mdp))
+    connexion.commit()
+
+def delete_user(id):
+    connexion = sqlite3.connect("./BDD/bdd.db")
+    curseur = connexion.cursor()
+    
+    curseur.execute("DELETE FROM user WHERE id = ?", (id,))
+    connexion.commit()
